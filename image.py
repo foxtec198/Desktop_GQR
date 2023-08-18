e93cb535dcdca1f9372255416b8603e58ff98053
foxtec198 <64221923+foxtec198@users.noreply.github.com>
2023-08-18 10:46:53 -0300

    .

--- image.py
+++ image.py
@@ -1,20 +1,21 @@
 from PIL import Image, ImageDraw, ImageFont
 
-def juntar():
-    modelo = Image.open('scr/MODELOO.png')
-    qr = Image.open('scr/qrteste.png')
-    merge = Image.new('RGBA', modelo.size)
-    x = int((modelo.size[0]-qr.size[0])/2)
-    merge.paste(modelo)
-    merge.paste(qr, (x, 300))
-    merge.save('teste.png')
-    
-tt = Image.new('RGBA', (200,100))
-d = ImageDraw.Draw(tt)
-fnt = ImageFont.truetype('scr/arial_narrow_7.ttf',30)
-text = 'TESTE PORTARIA ANALISA'
-an = text.split()
-an.insert(2, '\n')
-d.text((0,0), an, fill='black', font=fnt, align='center')
+text = 'CHORRASCARIA'
+qr = Image.open('scr/qrteste.png')
 
+modelo = Image.open('scr/MODELOO.png')
+merge = Image.new('RGBA', modelo.size)
+x = int((modelo.size[0]-qr.size[0])/2)
+merge.paste(modelo)
+merge.paste(qr, (x, 300))
+merge.save('teste.png')
+
+textoImg = Image.open('scr/blank.png')
+addText = ImageDraw.Draw(textoImg)
+fonte = ImageFont.truetype('scr/arial_narrow_7.ttf', 50)
+addText.text((0,30), text, fill='Black', font=fonte)
+x2 = int((modelo.size[0]-textoImg.size[0])/2)
+merge.paste(textoImg, (x2, 200))
+merge.show()
+print(qr.size[0])
 
