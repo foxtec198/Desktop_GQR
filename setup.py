0f024038f8d35c75f65186f970994dc7dd3bd545
foxtec198 <64221923+foxtec198@users.noreply.github.com>
2023-12-07 12:01:21 -0300

    .

---
+++ setup.py
@@ -0,0 +1,31 @@
+import sys
+from cx_Freeze import setup, Executable
+
+build_exe_options = {
+    "packages":[
+        "os","time","sqlite3"
+    ],
+    "includes":[
+        "PyQt5",
+        "pyodbc",
+        "yaml",
+        "segno",
+        "PIL",
+        "PyPDF2",
+        "reportlab",
+        "qdarktheme",
+        "webbrowser",
+    ]
+}
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+    
+setup(
+    name = 'Gerador de QR Code',
+    version = '1.0',
+    description = 'Gera QR Codes para utilizacao',
+    options = {"build_exe":build_exe_options},
+    executables = [Executable('main.py', base=base)]
+)
