2a99b5a4271c782b568cd4c286dbfe5b35a8dbf6
foxtec198 <64221923+foxtec198@users.noreply.github.com>
2023-12-10 04:23:12 -0300

    /

--- teste.py
+++
@@ -1,9 +0,0 @@
-from win10toast import ToastNotifier
-
-n = ToastNotifier()
-n.show_toast(
-    'Teste de Notify',
-    'Gerando qr codes',
-    duration=10,
-    icon_path="resources/scr/icon.ico"
-)
