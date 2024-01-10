eab7ebd67d310dea309c883c3928029f4ac61f1d
foxtec198 <64221923+foxtec198@users.noreply.github.com>
2024-01-10 18:15:01 -0300

    .

--- src/hotreload.py
+++ src/hotreload.py
@@ -1,4 +1,4 @@
-from kivymd.app import MDApp
+from kivymd.tools.hotreload.app import MDApp
 from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.screen import Screen
 from kivy.lang import Builder
@@ -10,18 +10,17 @@ class MainWin(Screen):
     ...
     
 class Main(MDApp):
-    def build(self):
-        super().__init__()
+    DEBUG = True
+    KV_FILES = ['src/style.kv']
+    
+    def build_app(self):
         Builder.load_file('src/style.kv')
         th = self.theme_cls
-        th.thme_style = 'Dark'
+        th.theme_style = 'Dark'
+        th.primary_palette = "Gray"
         sm = MDScreenManager()
         sm.add_widget(Login())
-        sm.add_widget(MainWin())
+        # sm.add_widget(MainWin())
         return sm
-
-    def on_start(self):
-        self.root.current = 'login'
-    
-if __name__ == '__main__':
-    Main().run()
+        
+Main().run()
