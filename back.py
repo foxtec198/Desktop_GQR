ce39ec32b529ea1afa952cbde9d2ce0cb3b48304
Guilherme Breve <64221923+foxtec198@users.noreply.github.com>
2024-02-08 01:55:08 -0300

    .

--- back.py
+++ back.py
@@ -1,5 +1,5 @@
 import pyodbc as sql
-from segno import make_qr
+from segno import make
 from functools import cache
 from os import mkdir, listdir, system, remove
 from time import strftime as st
@@ -22,109 +22,25 @@ class Logica:
         except: return 'CR não corresponde'
 
     @cache
-    def get_local(self, numCR, nivel = 3):
+    def get_local(self, numCR, nivel=3):
         try: return cons(f"SELECT Es.QRCode, Es.Descricao as 'Local', (SELECT Descricao FROM Estrutura Es2 WHERE Es2.Id = Es.EstruturaSuperiorId) as 'Superior' FROM Estrutura Es WHERE HierarquiaDescricao LIKE '%{numCR} -%' AND Nivel >= {nivel}")
         except: return "Erro com a consulta!"
 
     @cache
-    def set_dataframe(self, cr, nivel = 3):
+    def set_dataframe(self, cr, nivel=3):
         self.qrs = list() # Cria lista de QRs
         self.local_completo = list() #Cria Lista de Locais Completos
         estrutra = dict()
 
         cns = self.get_local(cr, nivel) # Consulta os Locais
         for row in cns: # Filtra os dados e manda para as respectivas listas
-            self.qrs.append(row[0])
-            self.local_completo.append(f'{row[2]} > {row[1]}')
+            print(row)
+            # self.qrs.append(row[0])
+            # self.local_completo.append(f'{row[2]} > {row[1]}')
             
         estrutra['QR'] = self.qrs
         estrutra['Local'] = self.local_completo
         return estrutra
-
-class QRCode:
-    def __init__(self):
-        self.lgc = Logica()
-        try: 
-            mkdir('src/temp')
-            mkdir('./QRCodes')
-        except: ...
-
-    @cache
-    def gerar_qr(self, modelo, cr, nivel = 3):
-        if modelo != '':
-            self.modelo = modelo
-            if cr != '':
-                if nivel == None or nivel == '':
-                    nivel = 3
-                else: 
-                    nivel = int(nivel)
-                    nivel += 3
-                estrutura = self.lgc.set_dataframe(cr, nivel)
-                cr = self.lgc.get_cr(cr)
-
-                locais = estrutura['Local']
-                qrs = estrutura['QR']
-                row = 0
-                for local in locais:
-                    imgQR = make_qr(qrs[row])
-                    imgQR.save('src/temp/qr.png', scale=10)
-
-                    # Abrindo as Imagens
-                    imgQR = Image.open('src/temp/qr.png')
-                    modelo = Image.open(f'src/{self.modelo}.png')
-                    modeloTxt = Image.open('src/600.png')
-                    
-                    # MONTAGEM
-                    novaImagem = Image.new('RGBA', modelo.size)
-                    novaImagem.paste(modelo)
-                    novaImagem.paste(imgQR, (int((modelo.width - imgQR.width)/2), 350))
-
-                    # TEXTO
-                    dw = ImageDraw.Draw(modeloTxt)
-                    fnt = ImageFont.truetype('src/arial_narrow_7.ttf', 35)
-                    tamanho_do_texto = dw.textlength(local, fnt)
-                    x = int((modeloTxt.width - tamanho_do_texto)/2)
-                    dw.text((x, 40), local, font=fnt, fill='black', align='center')
-                    modeloTxt.save('src/temp/text.png')
-
-                    # Finalizando Montagem
-                    imageTxt = Image.open('src/temp/text.png')
-                    novaImagem.paste(imageTxt, (int((modelo.size[0] - imageTxt.size[0])/2), 200))
-                    novaImagem.save('temp.png')
-
-                    # Cria PDFs temporarios
-                    try: mkdir('src/temp/pdfs')
-                    except: ...
-                    imgQr = Image.open('temp.png')
-                    nomePdf = f'src/temp/pdfs/{row}.pdf'
-                    arquivo = canvas.Canvas(nomePdf, pagesize=imgQr.size)
-                    arquivo.drawImage('temp.png', 0, 0)
-                    arquivo.save()
-                    row += 1
-
-                self.merge(cr) # Junção dos PDF's
-                self.remove_files('src/temp') # Delete dos arquivos temporarios
-                return f'QRCodes gerados com sucesso - {cr}'
-            else: return 'CR em branco'
-        else: return 'Problemas com os modelos!'
-
-    @cache
-    def merge(self, cr):
-        tm  = st('%d-%m-%Y - %H-%M-%S')
-        dir = listdir('src/temp/pdfs')
-        PDF = PdfMerger()
-        for i in dir:
-            if '.pdf' in i:
-                with open(f'src/temp/pdfs/{i}', 'rb') as pdf:
-                    leitura = PdfReader(pdf)
-                    PDF.append(leitura)
-        PDF.write(f'QRCodes/{cr} {tm}.pdf')
-        PDF.close()
-
-    @cache
-    def remove_files(self, dir):
-        try: rmtree(dir)
-        except: return 'Pasta não encontrada'
 
 class BackEnd:
     def __init__(self):
@@ -170,21 +86,82 @@ class BackEnd:
         self.c.execute(f'INSERT INTO USERS(user, pwd, servidor) VALUES ("{uid}","{pwd}","{server}")')
         self.lgConn.commit()
 
-    @cache
     def abrir_pasta_de_geração(self):
         try: mkdir(r'QRCodes')
         except: ...
         system('Explorer QRCodes')
 
-    @cache    
     def abrir_gitHub(self):
         on('https://github.com/foxtec198/Desktop_GQR/issues/new')
     
-    @cache
     def abrir_youtube(self):
         on('https://youtu.be/W6hMMplTn0Q')
 
+class QRCode:
+    def __init__(self) -> None:
+        try: mkdir('src/temp')
+        except: ...
+
+    def resizeImg(self, img, valor: tuple):
+        imgR = Image.open(img)
+        imgR.thumbnail(valor)
+        imgR.save(img)
+
+    def gerar_qr(self, cr, nivel=3):
+        if cr != '':
+            if nivel == '' or nivel == None:
+                nivel = 3
+            else: 
+                nivel = int(nivel)
+                nivel += 3
+
+            estrutra = l.set_dataframe(cr, nivel)
+            cr =   l.get_cr(cr)
+            qrs = estrutra['QR']
+            locais = estrutra['Local']
+            row = 0
+            for local in locais:
+                self.makePng(cr, local, qrs[row], 'teste')
+                row += 1
+    
+    def makePng(self, cr, local, qr, link):
+            # Gera os QR Codes
+            qrlocal = make(qr)
+            qrlast = make(link)
+
+            qrlocal.save('src/temp/qrtemp.png', scale=10)
+            qrlast.save('src/temp/qrtemp2.png', scale=10)
+
+            self.resizeImg('src/temp/qrtemp.png', (260, 260))
+            self.resizeImg('src/temp/qrtemp2.png', (150, 150))
+
+            coresImg = Image.open('src/cores/modeloVerde.png')
+            logoImg = Image.open('src/logos/ggps.png')
+            qrImg = Image.open('src/temp/qrtemp.png')
+            qrImg2 = Image.open('src/temp/qrtemp2.png')
+
+
+            # TEXTO - Nome CR
+            textImg = ImageDraw.Draw(coresImg)
+            fnt = ImageFont.truetype('src/arial_narrow_7.ttf', 30)
+            textImg.text((500, 150), cr, font=fnt, fill='black', align='center')
+
+            # TEXTO - Nome Local
+            textImg = ImageDraw.Draw(coresImg)
+            fnt = ImageFont.truetype('arial', 25)
+            textImg.text((500, 200), local, font=fnt, fill='black', align='center')
+
+            newImage = Image.new('RGBA', coresImg.size)
+            newImage.paste(coresImg)
+            newImage.paste(logoImg, (420, 5))
+            newImage.paste(qrImg, (75, 100))
+            newImage.paste(qrImg2, (755, 410))
+            newImage.save('temp.png')
+
+
 if __name__ == '__main__':
-    # BackEnd().login_sql('10.56.6.56', 'guilherme.breve','8458Guilherme')
-    # QRCode().gerar_qr(17739, 5)
-    BackEnd().cons()
+    b = BackEnd()
+    q = QRCode()
+    l = Logica()
+    print(b.login_sql('10.56.6.56','guilherme.breve','8458Guilherme'))
+    # q.gerar_qr(17739, 2)
